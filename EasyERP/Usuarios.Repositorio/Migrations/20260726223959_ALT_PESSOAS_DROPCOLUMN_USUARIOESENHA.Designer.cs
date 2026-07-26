e6f1b0729396b49d250ce4103a47bd4db69754b4
Matheus Faé <matheusfae@gmail.com>
2026-07-26 19:48:49 -0300

    Migration: removidas colunas adicionadas incorretamente

--- EasyERP/Usuarios.Repositorio/Migrations/20260726223959_ALT_PESSOAS_DROPCOLUMN_USUARIOESENHA.Designer.cs
+++ EasyERP/Usuarios.Repositorio/Migrations/20260726223959_ALT_PESSOAS_DROPCOLUMN_USUARIOESENHA.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Usuarios.Repositorio;
 
@@ -11,9 +12,11 @@ using Usuarios.Repositorio;
 namespace Usuarios.Repositorio.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260726223959_ALT_PESSOAS_DROPCOLUMN_USUARIOESENHA")]
+    partial class ALT_PESSOAS_DROPCOLUMN_USUARIOESENHA
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
