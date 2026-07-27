25325396162eb147552df8d6240fd95eaa4f4db9
Matheus Faé <matheusfae@gmail.com>
2026-07-27 11:00:27 -0300

    Refatoração no usuário e adição da entidade auditável no retorno

--- EasyERP/Model/DTOs/Usuario/UsuarioRespostaDTO.cs
+++ EasyERP/Model/DTOs/Usuario/UsuarioRespostaDTO.cs
@@ -1,41 +1,17 @@
-﻿using CrossCutting.Model.Enums;
+﻿using CrossCutting.Auditoria;
+using CrossCutting.Model.Enums;
 using Model.DTOs.Endereco;
+using Model.DTOs.PessoaFisica;
 
 namespace Model.DTOs.Usuario
 {
-    public class UsuarioRespostaDTO
+    public class UsuarioRespostaDTO : EntidadeAuditavel
     {
-        private string _cpf = string.Empty;
-        private string _telefone = string.Empty;
-        private string _genero = string.Empty;
-
-        public int Id { get; set; } // APENAS DEV, REMOVER
+        public int Id { get; set; } // TODO: REMOVER
         public Guid PublicId { get; set; }
-        public string NomeCompleto { get; set; } = string.Empty;
         public string NomeUsuario { get; set; } = string.Empty;
-        public string Genero
-        {
-            get => _genero; set => _genero = value switch
-            {
-                "M" => "Masculino",
-                "F" => "Feminino",
-                _ => "Outro/Não informado"
-            };
-        }
-        public string CPF
-        {
-            get => _cpf;
-            set => _cpf = value ?? Convert.ToUInt64(value).ToString(@"000\.000\.000\-00");
-        }
-
-        public string Telefone
-        {
-            get => _telefone;
-            set => _telefone = value ?? Convert.ToUInt64(value).ToString(@"(000) 00000-0000");
-        }
-        public string Email { get; set; } = string.Empty;
-        public DateTime DataNascimento { get; set; }
         public Perfil Perfil { get; set; }
         public EnderecoRespostaDTO? Endereco { get; set; }
+        public PessoaFisicaRespostaDTO? Pessoa { get; set; }
     }
 }
