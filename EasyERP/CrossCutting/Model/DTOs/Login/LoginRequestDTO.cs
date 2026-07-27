8f9ff19318a6188eb70e841f5130ec26cd4530b9
Matheus Faé <matheusfae@gmail.com>
2026-07-27 20:28:19 -0300

    Criados DTOs de Login

---
+++ EasyERP/CrossCutting/Model/DTOs/Login/LoginRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrossCutting.Model.DTOs.Acesso
+{
+    public class LoginRequestDTO
+    {
+        [Required(ErrorMessage = "Insira seu usuário ou e-mail")]
+        public string Login { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Insira sua senha")]
+        public string Senha { get; set; } = string.Empty;
+    }
+}
---
+++ EasyERP/CrossCutting/Model/DTOs/Login/LoginResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using Model.DTOs.Usuario;
+
+namespace CrossCutting.Model.DTOs.Login
+{
+    public class LoginResponseDTO
+    {
+        public string Token { get; set; }
+        public DateTime Expiracao { get; set; }
+        public UsuarioRespostaDTO Usuario { get; set; }
+    }
+}
