7d643b2914043e2773b16a328ef3c304903031b6
Matheus Faé <matheusfae@gmail.com>
2026-07-27 12:05:01 -0300

    Migration: Adicionado UK em PublicIds

---
+++ EasyERP/Auth.Repositorio/Migrations/20260727144838_SET_PUBLICID_UK.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auth.Repositorio.Migrations
+{
+    /// <inheritdoc />
+    public partial class SET_PUBLICID_UK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_USUARIOS_PublicId",
+                table: "USUARIOS",
+                column: "PublicId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PESSOASJURIDICAS_PublicId",
+                table: "PESSOASJURIDICAS",
+                column: "PublicId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PESSOAS_PublicId",
+                table: "PESSOAS",
+                column: "PublicId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_USUARIOS_PublicId",
+                table: "USUARIOS");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PESSOASJURIDICAS_PublicId",
+                table: "PESSOASJURIDICAS");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PESSOAS_PublicId",
+                table: "PESSOAS");
+        }
+    }
+}
