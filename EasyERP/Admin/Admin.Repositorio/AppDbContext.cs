b670c52b55f01550b4ce99f9e9c10204d6da8b90
Matheus Faé <matheusfae@gmail.com>
2026-08-07 17:31:58 -0300

    Criada estrutura base da api

---
+++ EasyERP/Admin/Admin.Repositorio/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Admin.Repositorio.Entidades;
+using CrossCutting.Auditoria;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace Admin.Repositorio;
+
+public partial class AppDbContext : DbContextAuditavel
+{
+    public AppDbContext(DbContextOptions options, IHttpContextAccessor httpContextAccessor)
+        : base(options, httpContextAccessor)
+    {
+    }
+
+    public DbSet<ApiExterna> ApiExterna { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        OnModelCreatingPartial(modelBuilder);
+    }
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EasyERP/Admin/Admin.Servicos/Mapeamento/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace Admin.Servicos.Mapeamento;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        //CreateMap<PessoaFisica, PessoaFisicaAtualizacaoDTO>()
+        //    .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+        //CreateMap<PessoaFisica, UsuarioRespostaDTO>();
+    }
+}
