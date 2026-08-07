fe2f3e8c675e63552fb0525a337210b80354a499
Matheus Faé <matheusfae@gmail.com>
2026-08-07 12:19:50 -0300

    Comentado temporariamente

--- EasyERP/CrossCutting/Bibliotecas/Http/ClientFactoryGet.cs
+++ EasyERP/CrossCutting/Bibliotecas/Http/ClientFactoryGet.cs
@@ -1,94 +1,94 @@
-﻿using Newtonsoft.Json;
+﻿//using Newtonsoft.Json;
 
-namespace Bibliotecas.Http;
+//namespace Bibliotecas.Http;
 
-public interface IClientFactoryGet
-{
-    Task<S> Get<S>(string endPoint, int apiId);
-    Task<S> Get<S>(string url, string endpoint, string token);
-}
+//public interface IClientFactoryGet
+//{
+//    Task<S> Get<S>(string endPoint, int apiId);
+//    Task<S> Get<S>(string url, string endpoint, string token);
+//}
 
-public class ClientFactoryGet : IClientFactoryGet
-{
-    private readonly IHttpClientFactory _httpClientFactory;
-    private readonly IAPIsServicos _api;
+//public class ClientFactoryGet : IClientFactoryGet
+//{
+//    private readonly IHttpClientFactory _httpClientFactory;
+//    private readonly IAPIsServicos _api;
 
-    public ClientFactoryGet(IHttpClientFactory httpClientFactory, IAPIsServicos api)
-    {
-        _httpClientFactory = httpClientFactory;
-        _api = api;
-    }
+//    public ClientFactoryGet(IHttpClientFactory httpClientFactory, IAPIsServicos api)
+//    {
+//        _httpClientFactory = httpClientFactory;
+//        _api = api;
+//    }
 
-    public async Task<S?> Get<S>(string endPoint, int apiId)
-    {
-        try
-        {
-            var api = await _api.ObterPorCodigo(apiId); // devtools 151
-            var httpClient = _httpClientFactory.CreateClient(api.Url);
+//    public async Task<S?> Get<S>(string endPoint, int apiId)
+//    {
+//        try
+//        {
+//            var api = await _api.ObterPorCodigo(apiId); // devtools 151
+//            var httpClient = _httpClientFactory.CreateClient(api.Url);
 
-            httpClient.DefaultRequestHeaders.Add("X-API-Key", api.Token);
+//            httpClient.DefaultRequestHeaders.Add("X-API-Key", api.Token);
 
-            string url = $"{api.Url}{endPoint}";
+//            string url = $"{api.Url}{endPoint}";
 
-            httpClient.DefaultRequestHeaders.Accept.Clear();
+//            httpClient.DefaultRequestHeaders.Accept.Clear();
 
-            using (HttpResponseMessage response = await httpClient.GetAsync(url))
-            {
-                try
-                {
-                    response.EnsureSuccessStatusCode();
-                    string json = await response.Content.ReadAsStringAsync();
-                    if (string.IsNullOrWhiteSpace(json))
-                    {
-                        return default;
-                    }
-                    return JsonConvert.DeserializeObject<S>(json);
-                }
-                catch (Exception)
-                {
-                    throw await ExceptionCustom.Exception(response);
-                }
-            }
-        }
-        catch (Exception)
-        {
-            throw;
-        }
-    }
+//            using (HttpResponseMessage response = await httpClient.GetAsync(url))
+//            {
+//                try
+//                {
+//                    response.EnsureSuccessStatusCode();
+//                    string json = await response.Content.ReadAsStringAsync();
+//                    if (string.IsNullOrWhiteSpace(json))
+//                    {
+//                        return default;
+//                    }
+//                    return JsonConvert.DeserializeObject<S>(json);
+//                }
+//                catch (Exception)
+//                {
+//                    throw await ExceptionCustom.Exception(response);
+//                }
+//            }
+//        }
+//        catch (Exception)
+//        {
+//            throw;
+//        }
+//    }
 
-    public async Task<S> Get<S>(string url, string endpoint, string token)
-    {
-        try
-        {
-            var httpClient = _httpClientFactory.CreateClient(url);
+//    public async Task<S> Get<S>(string url, string endpoint, string token)
+//    {
+//        try
+//        {
+//            var httpClient = _httpClientFactory.CreateClient(url);
 
-            httpClient.DefaultRequestHeaders.Add("X-API-Key", token);
+//            httpClient.DefaultRequestHeaders.Add("X-API-Key", token);
 
-            string urlCompleta = $"{url}{endpoint}";
+//            string urlCompleta = $"{url}{endpoint}";
 
-            httpClient.DefaultRequestHeaders.Accept.Clear();
+//            httpClient.DefaultRequestHeaders.Accept.Clear();
 
-            using (HttpResponseMessage response = await httpClient.GetAsync(urlCompleta))
-            {
-                try
-                {
-                    response.EnsureSuccessStatusCode();
-                    string json = await response.Content.ReadAsStringAsync();
-                    if (string.IsNullOrWhiteSpace(json))
-                    {
-                        return default;
-                    }
-                    return JsonConvert.DeserializeObject<S>(json);
-                }
-                catch (Exception)
-                {
-                    throw await ExceptionCustom.Exception(response);
-                }
-            }
-        }
-        catch (Exception)
-        {
-            throw;
-        }
-    }
-}
+//            using (HttpResponseMessage response = await httpClient.GetAsync(urlCompleta))
+//            {
+//                try
+//                {
+//                    response.EnsureSuccessStatusCode();
+//                    string json = await response.Content.ReadAsStringAsync();
+//                    if (string.IsNullOrWhiteSpace(json))
+//                    {
+//                        return default;
+//                    }
+//                    return JsonConvert.DeserializeObject<S>(json);
+//                }
+//                catch (Exception)
+//                {
+//                    throw await ExceptionCustom.Exception(response);
+//                }
+//            }
+//        }
+//        catch (Exception)
+//        {
+//            throw;
+//        }
+//    }
+//}
--- EasyERP/CrossCutting/Bibliotecas/Http/ClientFactoryPut.cs
+++ EasyERP/CrossCutting/Bibliotecas/Http/ClientFactoryPut.cs
@@ -1,76 +1,76 @@
-﻿using Newtonsoft.Json;
-using System.Text;
+﻿//using Newtonsoft.Json;
+//using System.Text;
 
-namespace Bibliotecas.Http;
+//namespace Bibliotecas.Http;
 
-public interface IClientFactoryPut
-{
-    Task<S?> Put<T, S>(string endPoint, T body, int apiId);
-    Task Put<T>(string endPoint, T body, int apiId);
-}
+//public interface IClientFactoryPut
+//{
+//    Task<S?> Put<T, S>(string endPoint, T body, int apiId);
+//    Task Put<T>(string endPoint, T body, int apiId);
+//}
 
-public class ClientFactoryPut : IClientFactoryPut
-{
-    private readonly IHttpClientFactory _httpClientFactory;
-    private readonly IAPIsServicos _api;
+//public class ClientFactoryPut : IClientFactoryPut
+//{
+//    private readonly IHttpClientFactory _httpClientFactory;
+//    private readonly IAPIsServicos _api;
 
-    public ClientFactoryPut(IHttpClientFactory httpClientFactory, IAPIsServicos api)
-    {
-        _httpClientFactory = httpClientFactory;
-        _api = api;
-    }
+//    public ClientFactoryPut(IHttpClientFactory httpClientFactory, IAPIsServicos api)
+//    {
+//        _httpClientFactory = httpClientFactory;
+//        _api = api;
+//    }
 
-    public async Task<S?> Put<T, S>(string endPoint, T body, int apiId)
-    {
-        var api = await _api.ObterPorCodigo(apiId); // devtools 151
+//    public async Task<S?> Put<T, S>(string endPoint, T body, int apiId)
+//    {
+//        var api = await _api.ObterPorCodigo(apiId); // devtools 151
 
-        var httpClient = _httpClientFactory.CreateClient();
-        httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-API-Key", api.Token);
+//        var httpClient = _httpClientFactory.CreateClient();
+//        httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-API-Key", api.Token);
 
-        string url = $"{api.Url}{endPoint}";
+//        string url = $"{api.Url}{endPoint}";
 
-        string jsonBody = JsonConvert.SerializeObject(body);
-        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+//        string jsonBody = JsonConvert.SerializeObject(body);
+//        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
 
-        using HttpResponseMessage response = await httpClient.PutAsync(url, content);
+//        using HttpResponseMessage response = await httpClient.PutAsync(url, content);
 
-        try
-        {
-            response.EnsureSuccessStatusCode();
+//        try
+//        {
+//            response.EnsureSuccessStatusCode();
 
-            string json = await response.Content.ReadAsStringAsync();
+//            string json = await response.Content.ReadAsStringAsync();
 
-            return string.IsNullOrWhiteSpace(json)
-                ? default
-                : JsonConvert.DeserializeObject<S>(json);
-        }
-        catch (HttpRequestException)
-        {
-            throw await ExceptionCustom.Exception(response);
-        }
-    }
+//            return string.IsNullOrWhiteSpace(json)
+//                ? default
+//                : JsonConvert.DeserializeObject<S>(json);
+//        }
+//        catch (HttpRequestException)
+//        {
+//            throw await ExceptionCustom.Exception(response);
+//        }
+//    }
 
-    public async Task Put<T>(string endPoint, T body, int apiId)
-    {
-        var api = await _api.ObterPorCodigo(apiId); // devtools 151
+//    public async Task Put<T>(string endPoint, T body, int apiId)
+//    {
+//        var api = await _api.ObterPorCodigo(apiId); // devtools 151
 
-        var httpClient = _httpClientFactory.CreateClient();
-        httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-API-Key", api.Token);
+//        var httpClient = _httpClientFactory.CreateClient();
+//        httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-API-Key", api.Token);
 
-        string url = $"{api.Url}{endPoint}";
+//        string url = $"{api.Url}{endPoint}";
 
-        string jsonBody = JsonConvert.SerializeObject(body);
-        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+//        string jsonBody = JsonConvert.SerializeObject(body);
+//        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
 
-        using HttpResponseMessage response = await httpClient.PutAsync(url, content);
+//        using HttpResponseMessage response = await httpClient.PutAsync(url, content);
 
-        try
-        {
-            response.EnsureSuccessStatusCode();
-        }
-        catch (HttpRequestException)
-        {
-            throw await ExceptionCustom.Exception(response);
-        }
-    }
-}
+//        try
+//        {
+//            response.EnsureSuccessStatusCode();
+//        }
+//        catch (HttpRequestException)
+//        {
+//            throw await ExceptionCustom.Exception(response);
+//        }
+//    }
+//}
