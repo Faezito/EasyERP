699b95e2b4d5ee440b0b18ea9c4ecf40d811d149
Matheus Faé <matheusfae@gmail.com>
2026-08-08 15:45:15 -0300

    Estrutura inicial da API

---
+++ EasyERP/Escolar/Escolar.API/Program.cs
@@ -0,0 +1,90 @@
+using AutoMapper;
+using Escolar.Repositorio;
+using Escolar.Servicos.Mapeamento;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Middlewares.Erro;
+using Serilog;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File(
+        path: "Logs/log-.txt",
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 30,
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+    )
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.InjecaoServicos();
+builder.Services.InjecaoRepositorios();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DB")));
+
+var config = new MapperConfiguration(
+    cfg =>
+    {
+        cfg.AddProfile<MappingProfile>();
+    },
+    new Microsoft.Extensions.Logging.Abstractions.NullLoggerFactory()
+);
+var mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "EasyERP.Escolar API V1");
+        c.RoutePrefix = string.Empty;
+        c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+    });
+}
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ EasyERP/Escolar/Escolar.API/RegistrosDI.cs
@@ -0,0 +1,19 @@
+﻿using Escolar.Repositorio;
+
+public static class RegistrosDI
+{
+    public static IServiceCollection InjecaoServicos(this IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+
+        //services.AddScoped<IApiExternaServicos, ApiExternaServicos>();
+
+        return services;
+    }
+
+    public static IServiceCollection InjecaoRepositorios(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(ICRUDGenerico<>), typeof(CRUDGenerico<>));
+        return services;
+    }
+}
