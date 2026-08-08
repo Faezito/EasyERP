f85a1d687b00eeb32331f3504db58712b11f9ca1
Matheus Faé <matheusfae@gmail.com>
2026-08-08 15:45:41 -0300

    Criação da camada de serviços + mapeamentos

---
+++ EasyERP/Escolar/Escolar.Servicos/Mapeamento/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Escolar.Repositorio.Entidades;
+using Model.DTOs.Escolar.Pessoa;
+
+namespace Escolar.Servicos.Mapeamento;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Pessoa, PessoaCadastroDTO>();
+        CreateMap<PessoaCadastroDTO, Pessoa>();
+
+        CreateMap<PessoaAtualizacaoDTO, Pessoa>();
+        CreateMap<Pessoa, PessoaAtualizacaoDTO>()
+            .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+    }
+}
