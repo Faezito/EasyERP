9186d0140103f5ee1b3e72197c4699ce7d892683
Matheus Faé <matheusfae@gmail.com>
2026-08-08 16:22:32 -0300

    CRUD base de Turma concluido

---
+++ EasyERP/Escolar/Escolar.API/Controllers/TurmaController.cs
@@ -0,0 +1,47 @@
+﻿using Escolar.Servicos;
+using Microsoft.AspNetCore.Mvc;
+using Model.DTOs.Escolar.Turma;
+
+namespace Escolar.API.Controllers;
+
+[ApiController]
+[Route("api/escolar/[controller]")]
+public class TurmaController(ITurmaServicos turmaServicos) : ControllerBase
+{
+    private readonly ITurmaServicos turmaServicos = turmaServicos;
+
+    [HttpGet("{turmaId}")]
+    public async Task<IActionResult> Obter(int turmaId)
+    {
+        var turma = await turmaServicos.ObterPorIdAsync(turmaId);
+        return Ok(turma);
+    }
+
+    [HttpGet("listar")]
+    public async Task<IActionResult> Listar()
+    {
+        var turmas = await turmaServicos.ObterTodosAsync();
+        return Ok(turmas);
+    }
+
+    [HttpPost("cadastrar")]
+    public async Task<IActionResult> Cadastrar(TurmaDTO dto)
+    {
+        await turmaServicos.Cadastrar(dto);
+        return Ok();
+    }
+
+    [HttpPut("atualizar")]
+    public async Task<IActionResult> Atualizar(TurmaDTO dto)
+    {
+        await turmaServicos.Atualizar(dto);
+        return Ok();
+    }
+
+    [HttpDelete("{turmaId}")]
+    public async Task<IActionResult> Deletar(int turmaId)
+    {
+        await turmaServicos.RemoverAsync(new Repositorio.Entidades.Turma { Id = turmaId });
+        return Ok();
+    }
+}
---
+++ EasyERP/Escolar/Escolar.Servicos/TurmaServicos.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Escolar.Repositorio;
+using Escolar.Repositorio.Entidades;
+using Model.DTOs.Escolar.Turma;
+
+namespace Escolar.Servicos;
+
+public interface ITurmaServicos : ICRUDGenerico<Turma>
+{
+    Task Cadastrar(TurmaDTO dto);
+    Task Atualizar(TurmaDTO dto);
+}
+
+public class TurmaServicos : CRUDGenerico<Turma>, ITurmaServicos
+{
+    public TurmaServicos(AppDbContext db, IMapper mapper) : base(db, mapper) {}
+
+    public async Task Atualizar(TurmaDTO dto)
+    {
+        var turma = _mapper.Map<Turma>(dto);
+        await AtualizarAsync(turma);
+    }
+
+    public async Task Cadastrar(TurmaDTO dto)
+    {
+        var turma = _mapper.Map<Turma>(dto);
+        await AdicionarAsync(turma);
+    }
+}
