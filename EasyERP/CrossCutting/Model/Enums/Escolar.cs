33ea219be77c96f6d85051cce3aeafc8a658a098
Matheus Faé <matheusfae@gmail.com>
2026-08-08 15:47:38 -0300

    Criado enum de TiposDePessoas no modulo escolar

---
+++ EasyERP/CrossCutting/Model/Enums/Escolar.cs
@@ -0,0 +1,12 @@
+﻿namespace CrossCutting.Model.Enums;
+
+public enum TipoDePessoa
+{
+    Admin = 1,
+    Diretoria = 2,
+    Coordenacao = 3,
+    Administrativo = 4,
+    Professor = 5,
+    Responsavel = 6,
+    Aluno = 10
+}
