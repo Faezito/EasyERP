be99f06750765e942b6f07b23b46afcb99fd3deb
Matheus Faé <matheusfae@gmail.com>
2026-08-08 15:46:38 -0300

    Criação da camada de Dados + contextos base

---
+++ EasyERP/Escolar/Escolar.Repositorio/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CrossCutting.Auditoria;
+using Escolar.Repositorio.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace Escolar.Repositorio;
+
+public partial class AppDbContext : DbContextAuditavel
+{
+    public AppDbContext(DbContextOptions options, IHttpContextAccessor httpContextAccessor)
+        : base(options, httpContextAccessor)
+    {
+    }
+
+    public DbSet<Pessoa> Pessoas { get; set; }
+    public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<Disciplina> Disciplinas { get; set; }
+    public DbSet<Nota> Notas { get; set; }
+    public DbSet<Turma> Turmas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        OnModelCreatingPartial(modelBuilder);
+    }
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EasyERP/Escolar/Escolar.Repositorio/CRUDGenerico.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Escolar.Repositorio;
+
+public interface ICRUDGenerico<T> where T : class
+{
+    Task<T?> ObterPorIdAsync(int id);
+    Task<List<T>> ObterTodosAsync();
+    Task AdicionarAsync(T entity);
+    void Atualizar(T entity);
+    void Adicionar(T entity);
+    void Remover(T entity);
+    void BulkRemove(List<T> entities);
+    Task RemoverAsync(T entity);
+    Task SalvarAsync();
+}
+
+public class CRUDGenerico<T> : ICRUDGenerico<T> where T : class
+{
+    protected readonly AppDbContext _db;
+    protected readonly DbSet<T> _dbSet;
+
+    public CRUDGenerico(AppDbContext db)
+    {
+        _db = db;
+        _dbSet = db.Set<T>();
+    }
+
+    public async Task<T?> ObterPorIdAsync(int id)
+        => await _dbSet.FindAsync(id);
+
+    public async Task<List<T>> ObterTodosAsync()
+        => await _dbSet.ToListAsync();
+
+    public async Task AdicionarAsync(T entity)
+    {
+        _dbSet.Add(entity);
+        await _db.SaveChangesAsync();
+    }
+
+    public void Adicionar(T entity)
+        => _dbSet.Add(entity);
+
+    public void Atualizar(T entity)
+        => _dbSet.Update(entity);
+
+    public void Remover(T entity)
+        => _dbSet.Remove(entity);
+
+    public async Task SalvarAsync()
+        => await _db.SaveChangesAsync();
+
+    public void BulkRemove(List<T> entities)
+    {
+        foreach (var entity in entities)
+            _dbSet.Remove(entity);
+    }
+
+    public async Task RemoverAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        await SalvarAsync();
+    }
+}
