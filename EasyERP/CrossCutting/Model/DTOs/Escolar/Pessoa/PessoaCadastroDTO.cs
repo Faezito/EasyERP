3e7b66f59f57a5d5e2cb6b631dcbfd8ed8b8ce5d
Matheus Faé <matheusfae@gmail.com>
2026-08-11 10:01:42 -0300

    Ajustes nos data annotations

--- EasyERP/CrossCutting/Model/DTOs/Escolar/Pessoa/PessoaCadastroDTO.cs
+++ EasyERP/CrossCutting/Model/DTOs/Escolar/Pessoa/PessoaCadastroDTO.cs
@@ -1,29 +1,58 @@
 ﻿using CrossCutting.Model.Enums;
 using Model.DTOs.Endereco;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace Model.DTOs.Escolar.Pessoa;
 
-public class PessoaRespostaDTO
+public class PessoaCadastroDTO
 {
     private string _cpf = string.Empty;
     private string _telefone = string.Empty;
 
-    public Guid PublicId { get; set; }
+    public Guid PublicId => Guid.CreateVersion7();
+
+    [Required(ErrorMessage = "Nome Completo é necessário")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O Nome Completo precisa conter entre 3 e 100 caracteres")]
+    [Display(Name = "Nome Completo")]
     public string NomeCompleto { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Gênero é necessário")]
+    [Display(Name = "Gênero")]
+    [RegularExpression("^[MFON]$", ErrorMessage = "O Gênero informado é inválido")]
     public string Genero { get; set; } = string.Empty;
+
+
+    [Required(ErrorMessage = "E-mail é necessário")]
+    [StringLength(150, ErrorMessage = "O E-mail deve conter no máximo 150 caracteres")]
+    [Display(Name = "E-mail")]
+    [EmailAddress(ErrorMessage = "O E-mail informado é inválido")]
     public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "CPF é necessário")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "O CPF deve conter 11 dígitos")]
+    [Display(Name = "CPF")]
     public string CPF
     {
         get => _cpf;
         set => _cpf = Regex.Replace(value ?? string.Empty, "[^0-9]", "");
     }
+
+
+    [Required(ErrorMessage = "Telefone é necessário")]
+    [StringLength(11, MinimumLength = 10, ErrorMessage = "O telefone deve conter 10 ou 11 dígitos")]
+    [Display(Name = "Telefone")]
     public string Telefone
     {
         get => _telefone;
         set => _telefone = Regex.Replace(value ?? string.Empty, "[^0-9]", "");
     }
+
+
+    [Required(ErrorMessage = "Data de Nascimento é necessária")]
+    [Display(Name = "Data de Nascimento")]
     public DateTime DataNascimento { get; set; }
+
     public TipoDePessoa Tipo { get; set; }
-    public EnderecoRespostaDTO Endereco { get; set; } = null!;
+    public EnderecoCadastroDTO? Endereco { get; set; }
 }
