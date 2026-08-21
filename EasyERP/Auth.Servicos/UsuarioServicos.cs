3144f7e132476a289fd09d83bc08bc15f519b5b2
Matheus Faé <matheusfae@gmail.com>
2026-08-21 13:25:06 -0300

    Alterado método de exclusão para deleção lógica

--- EasyERP/Auth.Servicos/UsuarioServicos.cs
+++ EasyERP/Auth.Servicos/UsuarioServicos.cs
@@ -5,98 +5,97 @@ using Microsoft.EntityFrameworkCore;
 using Model.DTOs.Usuario;
 using bC = BCrypt.Net.BCrypt;
 
-namespace Auth.Servicos
+namespace Auth.Servicos;
+
+public interface IUsuarioServicos : ICRUDGenerico<Usuario>
 {
-    public interface IUsuarioServicos : ICRUDGenerico<Usuario>
+    Task Cadastrar(UsuarioCadastroDTO dto);
+    Task CadastrarBulk(List<UsuarioCadastroDTO> dto);
+    Task Atualizar(UsuarioAtualizacaoDTO dto);
+    Task<UsuarioRespostaDTO> ObterPorId(int id);
+    Task<UsuarioRespostaDTO> ObterPorPublicId(Guid publicId);
+    Task<Usuario?> ObterPorLogin(string login);
+    Task<List<UsuarioRespostaDTO>> Listar();
+    Task Deletar(Guid publicId);
+}
+
+public class UsuarioServicos : CRUDGenerico<Usuario>, IUsuarioServicos
+{
+    private readonly IMapper _mapper;
+    public UsuarioServicos(AppDbContext db, IMapper mapper) : base(db)
     {
-        Task Cadastrar(UsuarioCadastroDTO dto);
-        Task CadastrarBulk(List<UsuarioCadastroDTO> dto);
-        Task Atualizar(UsuarioAtualizacaoDTO dto);
-        Task<UsuarioRespostaDTO> ObterPorId(int id);
-        Task<UsuarioRespostaDTO> ObterPorPublicId(Guid publicId);
-        Task<Usuario?> ObterPorLogin(string login);
-        Task<List<UsuarioRespostaDTO>> Listar();
-        Task Deletar(Guid publicId);
+        _mapper = mapper;
     }
 
-    public class UsuarioServicos : CRUDGenerico<Usuario>, IUsuarioServicos
+    public async Task Atualizar(UsuarioAtualizacaoDTO dto)
     {
-        private readonly IMapper _mapper;
-        public UsuarioServicos(AppDbContext db, IMapper mapper) : base(db)
+        var usuario = await _db.Usuarios.FirstOrDefaultAsync(x => x.PublicId == dto.PublicId)
+            ?? throw new Exception("Erro ao atualizar: usuário não encontrado.");
+
+        usuario.SenhaHash = string.IsNullOrWhiteSpace(dto.Senha) ? usuario.SenhaHash : bC.HashPassword(dto.Senha);
+        usuario.Perfil = dto.Perfil ?? usuario.Perfil;
+
+        await SalvarAsync();
+    }
+
+    public async Task Cadastrar(UsuarioCadastroDTO dto)
+    {
+        var usuario = _mapper.Map<Usuario>(dto);
+        var pessoa = _mapper.Map<PessoaFisica>(dto);
+
+        usuario.SenhaHash = bC.HashPassword(dto.Senha);
+        usuario.PessoaFisica = pessoa;
+
+        Adicionar(usuario);
+        await SalvarAsync();
+    }
+
+    public async Task CadastrarBulk(List<UsuarioCadastroDTO> dto)
+    {
+        foreach (var item in dto)
         {
-            _mapper = mapper;
-        }
+            var usuario = _mapper.Map<Usuario>(item);
+            var pessoa = _mapper.Map<PessoaFisica>(item);
 
-        public async Task Atualizar(UsuarioAtualizacaoDTO dto)
-        {
-            var usuario = await _db.Usuarios.FirstOrDefaultAsync(x => x.PublicId == dto.PublicId);
-
-            usuario.SenhaHash = string.IsNullOrWhiteSpace(dto.Senha) ? usuario.SenhaHash : bC.HashPassword(dto.Senha);
-            usuario.Perfil = dto.Perfil ?? usuario.Perfil;
-            usuario.AtualizadoEm = DateTime.Now;
-
-            await SalvarAsync();
-        }
-
-        public async Task Cadastrar(UsuarioCadastroDTO dto)
-        {
-            var usuario = _mapper.Map<Usuario>(dto);
-            var pessoa = _mapper.Map<PessoaFisica>(dto);
-
-            usuario.SenhaHash = bC.HashPassword(dto.Senha);
+            usuario.SenhaHash = bC.HashPassword(item.Senha);
             usuario.PessoaFisica = pessoa;
 
             Adicionar(usuario);
-            await SalvarAsync();
         }
+        await SalvarAsync();
+    }
 
-        public async Task CadastrarBulk(List<UsuarioCadastroDTO> dto)
-        {
-            foreach (var item in dto)
-            {
-                var usuario = _mapper.Map<Usuario>(item);
-                var pessoa = _mapper.Map<PessoaFisica>(item);
+    public async Task Deletar(Guid publicId)
+    {
+        var usuario = await _db.Usuarios.FirstOrDefaultAsync(x => x.PublicId == publicId);
+        if (usuario == null) throw new Exception("Erro ao excluir: Usuario não encontrado.");
 
-                usuario.SenhaHash = bC.HashPassword(item.Senha);
-                usuario.PessoaFisica = pessoa;
+        usuario.Deletar(); // Deleção lógica
+        await SalvarAsync();
+    }
 
-                Adicionar(usuario);
-            }
-            await SalvarAsync();
-        }
+    public async Task<List<UsuarioRespostaDTO>> Listar()
+    {
+        var usuarios = await ObterTodosAsync();
+        return _mapper.Map<List<UsuarioRespostaDTO>>(usuarios);
+    }
 
-        public async Task Deletar(Guid publicId)
-        {
-            var usuario = await _db.Usuarios.FirstOrDefaultAsync(x => x.PublicId == publicId);
-            if (usuario == null) throw new Exception("Erro ao excluir: Usuario não encontrado.");
+    public async Task<UsuarioRespostaDTO> ObterPorId(int id)
+    {
+        var usuario = await _db.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        return _mapper.Map<UsuarioRespostaDTO>(usuario);
+    }
 
-            _db.Remove(usuario);
-            await SalvarAsync();
-        }
+    public async Task<Usuario?> ObterPorLogin(string login)
+    {
+        return await _dbSet
+            .Include(x => x.PessoaFisica)
+            .FirstOrDefaultAsync(x => x.NomeUsuario == login || x.PessoaFisica.Email == login);
+    }
 
-        public async Task<List<UsuarioRespostaDTO>> Listar()
-        {
-            var usuarios = await ObterTodosAsync();
-            return _mapper.Map<List<UsuarioRespostaDTO>>(usuarios);
-        }
-
-        public async Task<UsuarioRespostaDTO> ObterPorId(int id)
-        {
-            var usuario = await _db.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
-            return _mapper.Map<UsuarioRespostaDTO>(usuario);
-        }
-
-        public async Task<Usuario?> ObterPorLogin(string login)
-        {
-            return await _dbSet
-                .Include(x => x.PessoaFisica)
-                .FirstOrDefaultAsync(x => x.NomeUsuario == login || x.PessoaFisica.Email == login);
-        }
-
-        public async Task<UsuarioRespostaDTO> ObterPorPublicId(Guid publicId)
-        {
-            var usuario = await _db.Usuarios.FirstOrDefaultAsync(x => x.PublicId == publicId);
-            return _mapper.Map<UsuarioRespostaDTO>(usuario);
-        }
+    public async Task<UsuarioRespostaDTO> ObterPorPublicId(Guid publicId)
+    {
+        var usuario = await _db.Usuarios.FirstOrDefaultAsync(x => x.PublicId == publicId);
+        return _mapper.Map<UsuarioRespostaDTO>(usuario);
     }
 }
