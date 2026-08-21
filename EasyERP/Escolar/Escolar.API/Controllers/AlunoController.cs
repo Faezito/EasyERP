0224e00b0f26182d361030117d4a560fb3a4bbfa
Matheus Faé <matheusfae@gmail.com>
2026-08-21 13:26:47 -0300

    Finalizado CRUD base de Aluno

---
+++ EasyERP/Escolar/Escolar.API/Controllers/AlunoController.cs
@@ -0,0 +1,40 @@
+﻿using CrossCutting.Model.DTOs.Escolar.Aluno;
+using Escolar.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Escolar.API.Controllers;
+
+[ApiController]
+[Route("api/escolar/[controller]")]
+public class AlunoController(IAlunoServicos alunoServicos) : ControllerBase
+{
+    private readonly IAlunoServicos _alunoServicos = alunoServicos;
+
+    [HttpGet("listar")]
+    public async Task<IActionResult> Listar()
+    {
+        var alunos = await _alunoServicos.ObterTodosAsync();
+        return Ok(alunos);
+    }
+
+    [HttpPost("cadastrar")]
+    public async Task<IActionResult> Cadastrar(AlunoCadastroDTO alunoDto)
+    {
+        await _alunoServicos.Cadastro(alunoDto);
+        return Ok();
+    }
+
+    [HttpPut("atualizar")]
+    public async Task<IActionResult> Atualizar(AlunoAtualizacaoDTO alunoDto)
+    {
+        await _alunoServicos.Atualizacao(alunoDto);
+        return Ok();
+    }
+
+    [HttpDelete("{publicId}")]
+    public async Task<IActionResult> Deletar(Guid publicId)
+    {
+        await _alunoServicos.Excluir(publicId);
+        return Ok();
+    }
+}
---
+++ EasyERP/Escolar/Escolar.Servicos/AlunoServicos.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using CrossCutting.Model.DTOs.Escolar.Aluno;
+using Escolar.Repositorio;
+using Escolar.Repositorio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Escolar.Servicos;
+
+public interface IAlunoServicos : ICRUDGenerico<Aluno>
+{
+    Task<List<AlunoRespostaDTO>> Listar();
+    Task Cadastro(AlunoCadastroDTO alunoDto);
+    Task Atualizacao(AlunoAtualizacaoDTO alunoDto);
+    Task Excluir(Guid publicId);
+}
+
+public class AlunoServicos(AppDbContext db, IMapper mapper) : CRUDGenerico<Aluno>(db, mapper), IAlunoServicos
+{
+    public async Task Atualizacao(AlunoAtualizacaoDTO alunoDto)
+    {
+        var aluno = await _dbSet.Include(x=>x.Pessoa).FirstOrDefaultAsync(x=>x.Pessoa.PublicId == alunoDto.PessoaId)
+            ?? throw new Exception("Erro ao atualizar: Aluno não encontrado");
+        var pessoa = aluno.Pessoa;
+
+        pessoa.NomeCompleto = string.IsNullOrWhiteSpace(alunoDto.NomeCompleto) ? pessoa.NomeCompleto : alunoDto.NomeCompleto;
+        pessoa.Genero = string.IsNullOrWhiteSpace(alunoDto.Genero) ? pessoa.Genero : alunoDto.Genero;
+        pessoa.Telefone = string.IsNullOrWhiteSpace(alunoDto.Telefone) ? pessoa.Telefone : alunoDto.Telefone;
+        pessoa.Email = string.IsNullOrWhiteSpace(alunoDto.Email) ? pessoa.Email : alunoDto.Email;
+
+        aluno.TurmaId = alunoDto.TurmaId ?? aluno.TurmaId;
+
+        await SalvarAsync();
+    }
+
+    public async Task Cadastro(AlunoCadastroDTO alunoDto)
+    {
+        var aluno = _mapper.Map<Aluno>(alunoDto);
+        await AdicionarAsync(aluno);
+    }
+
+    public async Task Excluir(Guid publicId)
+    {
+        var aluno = await ObterPorIdAsync(publicId) 
+            ?? throw new Exception("Erro ao deletar: aluno não encontrado.");
+        aluno.Deletar();
+        await SalvarAsync();
+    }
+
+    public async Task<List<AlunoRespostaDTO>> Listar()
+    {
+        var alunos = await _dbSet.AsNoTracking().ToListAsync();
+        return _mapper.Map<List<AlunoRespostaDTO>>(alunos);
+    }
+}
