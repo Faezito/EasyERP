6c93993dbca111deb955f0b34b578fb22e097651
Matheus Faé <matheusfae@gmail.com>
2026-08-21 15:37:11 -0300

    Criada classe de atributos de validação de data

---
+++ EasyERP/CrossCutting/Bibliotecas/Attributes/DataAttributes.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bibliotecas.Attributes;
+
+public class DataAttributes
+{
+    public class DataNaoFuturaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(
+            object? value,
+            ValidationContext validationContext)
+        {
+            if (value is DateTime data && data > DateTime.Now.AddMinutes(1))
+                return new ValidationResult("A data não pode ser futura.");
+
+            return ValidationResult.Success;
+        }
+    }
+
+    public class ValidarDataAttribute : ValidationAttribute
+    {
+        private readonly DateTime _dataMinima;
+        private readonly DateTime _dataMaxima;
+
+        public ValidarDataAttribute(string dataMinima = "01/01/1900", string dataMaxima = "31/12/2999")
+        {
+            _dataMinima = DateTime.Parse(dataMinima);
+            _dataMaxima = DateTime.Parse(dataMaxima);
+        }
+
+        protected override ValidationResult? IsValid(
+            object? value,
+            ValidationContext validationContext)
+        {
+            if (value is not DateTime data)
+                return ValidationResult.Success;
+
+            if (data < _dataMinima || data > _dataMaxima)
+            {
+                return new ValidationResult(
+                    $"Data inválida. A data precisa estar entre " +
+                    $"{_dataMinima:dd/MM/yyyy} e {_dataMaxima:dd/MM/yyyy}");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
