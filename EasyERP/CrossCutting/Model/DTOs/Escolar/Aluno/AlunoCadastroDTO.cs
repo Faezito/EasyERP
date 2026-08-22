cfe940b825c6699a548911a7f231f5de8dc0d6e7
Matheus Faé <matheusfae@gmail.com>
2026-08-22 17:50:28 -0300

    Correção no tipo da propriedade

--- EasyERP/CrossCutting/Model/DTOs/Escolar/Aluno/AlunoCadastroDTO.cs
+++ EasyERP/CrossCutting/Model/DTOs/Escolar/Aluno/AlunoCadastroDTO.cs
@@ -5,5 +5,5 @@ namespace CrossCutting.Model.DTOs.Escolar.Aluno;
 public class AlunoCadastroDTO
 {
     public PessoaCadastroDTO? Pessoa { get; set; }
-    public Guid? TurmaId { get; set; }
+    public int? TurmaId { get; set; }
 }
