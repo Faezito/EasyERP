45a3f6231c6fcbd87d254abed146ddd48b580c3e
Matheus Faé <matheusfae@gmail.com>
2026-08-24 18:07:06 -0300

    Criação base da área escolar

---
+++ EasyERP/Web/Areas/Escolar/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Escolar.Controllers;
+
+[Area("Escolar")]
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
