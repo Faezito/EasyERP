7c47594f3b503ec1d50e4bbaa1fd01a9551400b6
Matheus Faé <matheusfae@gmail.com>
2026-08-24 18:05:05 -0300

    Ativada classe de abstração de Post e criada classe para dados de Apis

---
+++ EasyERP/CrossCutting/Bibliotecas/Http/Api.cs
@@ -0,0 +1,7 @@
+﻿namespace Bibliotecas.Http;
+
+public class Api
+{
+    public string? Url { get; set; }
+    public string? Token { get; set; }
+}
--- EasyERP/CrossCutting/Bibliotecas/Http/ClientFactoryPost.cs
+++ EasyERP/CrossCutting/Bibliotecas/Http/ClientFactoryPost.cs
@@ -1,104 +1,100 @@
-﻿//using Newtonsoft.Json;
-//using System.Text;
+﻿using Newtonsoft.Json;
+using System.Text;
 
-//namespace Bibliotecas.Http;
+namespace Bibliotecas.Http;
 
-//public interface IClientFactoryPost
-//{
-//    Task<S> Post<S, E>(string endPoint, E model, int apiId);
-//    Task Post<E>(string endPoint, E model, int apiId);
-//}
+public interface IClientFactoryPost
+{
+    Task<S> Post<S, E>(string endPoint, E model, Api api);
+    Task Post<E>(string endPoint, E model, Api api);
+}
 
-//public class ClientFactoryPost : IClientFactoryPost
-//{
-//    private readonly IHttpClientFactory _httpClientFactory;
-//    private readonly IAPIsServicos _api;
+public class ClientFactoryPost : IClientFactoryPost
+{
+    private readonly IHttpClientFactory _httpClientFactory;
 
-//    public ClientFactoryPost(IHttpClientFactory httpClientFactory, IAPIsServicos api)
-//    {
-//        _httpClientFactory = httpClientFactory;
-//        _api = api;
-//    }
+    public ClientFactoryPost(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
 
-//    public async Task<S?> Post<S, E>(string endPoint, E model, int apiId)
-//    {
-//        try
-//        {
-//            var api = await _api.ObterPorCodigo(apiId); // devtools 151
-//            var httpClient = _httpClientFactory.CreateClient(api.Url);
+    public async Task<S?> Post<S, E>(string endPoint, E model, Api api)
+    {
+        try
+        {
+            var httpClient = _httpClientFactory.CreateClient(api.Url);
 
-//            httpClient.DefaultRequestHeaders.Add("X-API-Key", api.Token);
-//            string url = $"{api.Url}{endPoint}";
+            httpClient.DefaultRequestHeaders.Add("X-API-Key", api.Token);
+            string url = $"{api.Url}{endPoint}";
 
-//            httpClient.DefaultRequestHeaders.Accept.Clear();
-//            string strJson = JsonConvert.SerializeObject(model);
-//            StringContent httpContent = new(strJson, Encoding.UTF8, "application/json");
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            string strJson = JsonConvert.SerializeObject(model);
+            StringContent httpContent = new(strJson, Encoding.UTF8, "application/json");
 
-//            using (HttpResponseMessage response = await httpClient.PostAsync(url, httpContent))
-//            {
-//                try
-//                {
-//                    response.EnsureSuccessStatusCode();
-//                    string json = await response.Content.ReadAsStringAsync();
-//                    if (string.IsNullOrWhiteSpace(json))
-//                    {
-//                        return default;
-//                    }
-//                    return JsonConvert.DeserializeObject<S>(json);
-//                }
-//                catch (Exception ex)
-//                {
-//                    throw await ExceptionCustom.Exception(response);
-//                }
-//            }
-//        }
-//        catch (Exception)
-//        {
-//            throw;
-//        }
-//    }
+            using (HttpResponseMessage response = await httpClient.PostAsync(url, httpContent))
+            {
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    string json = await response.Content.ReadAsStringAsync();
+                    if (string.IsNullOrWhiteSpace(json))
+                    {
+                        return default;
+                    }
+                    return JsonConvert.DeserializeObject<S>(json);
+                }
+                catch (Exception ex)
+                {
+                    throw await ExceptionCustom.Exception(response);
+                }
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 
-//    public async Task Post<E>(string endPoint, E model, int apiId)
-//    {
-//        try
-//        {
-//            var api = await _api.ObterPorCodigo(apiId); // devtools 151
-//            var httpClient = _httpClientFactory.CreateClient();
+    public async Task Post<E>(string endPoint, E model, Api api)
+    {
+        try
+        {
+            var httpClient = _httpClientFactory.CreateClient();
 
-//            httpClient.DefaultRequestHeaders.Add("X-API-Key", api.Token);
+            httpClient.DefaultRequestHeaders.Add("X-API-Key", api.Token);
 
-//            //string Url = "http://localhost:8080";
+            //string Url = "http://localhost:8080";
 
-//            string url = $"{api.Url}{endPoint}";
+            string url = $"{api.Url}{endPoint}";
 
-//            var settings = new JsonSerializerSettings
-//            {
-//                DateFormatString = "yyyy-MM-dd'T'HH:mm:ss.fffZ",
-//                DateTimeZoneHandling = DateTimeZoneHandling.Utc
-//            };
+            var settings = new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-dd'T'HH:mm:ss.fffZ",
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc
+            };
 
-//            httpClient.DefaultRequestHeaders.Accept.Clear();
-//            StringContent httpContent = new(JsonConvert.SerializeObject(model), Encoding.UTF8, System.Net.Mime.MediaTypeNames.Application.Json);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            StringContent httpContent = new(JsonConvert.SerializeObject(model), Encoding.UTF8, System.Net.Mime.MediaTypeNames.Application.Json);
 
-//            var json = JsonConvert.SerializeObject(model, settings);
-//            Console.WriteLine(json);
+            var json = JsonConvert.SerializeObject(model, settings);
+            Console.WriteLine(json);
 
-//            using (HttpResponseMessage response = await httpClient.PostAsync(url, httpContent))
-//            {
-//                try
-//                {
-//                    response.EnsureSuccessStatusCode();
-//                }
-//                catch (Exception)
-//                {
-//                    throw await ExceptionCustom.Exception(response);
-//                }
-//            }
-//        }
-//        catch (Exception)
-//        {
-//            throw;
-//        }
-//    }
+            using (HttpResponseMessage response = await httpClient.PostAsync(url, httpContent))
+            {
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                }
+                catch (Exception)
+                {
+                    throw await ExceptionCustom.Exception(response);
+                }
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 
-//}
+}
