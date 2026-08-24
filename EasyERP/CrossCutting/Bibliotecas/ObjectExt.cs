6a12b1adc3f476bcab4efa02aa70459575b3c708
Matheus Faé <matheusfae@gmail.com>
2026-08-24 18:06:23 -0300

    Criada classe para manipulação de objetos e método de clonagem

---
+++ EasyERP/CrossCutting/Bibliotecas/ObjectExt.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Bibliotecas;
+
+public static class ObjectExt
+{
+    //
+    // Summary:
+    //     Clona o objeto.
+    //
+    // Parameters:
+    //   source:
+    //     O objeto a ser clonado.
+    //
+    // Type parameters:
+    //   T:
+    //     Tipo do objeto.
+    //
+    // Returns:
+    //     Nova instância do objeto clonado.
+    public static T Clonar<T>(this T source)
+    {
+        return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(source));
+    }
+}
