57bbc5387094fbbef252f51db84f286556d914e0
Matheus Faé <matheusfae@gmail.com>
2026-08-25 18:57:29 -0300

    Ajuste na nomenclatura dos componentes

---
+++ EasyERP/Web/Models/Components/BotaoComponent.cs
@@ -0,0 +1,42 @@
+﻿using Biblioteca;
+
+namespace Web.Models.Components;
+
+public class BotaoComponent
+{
+    public string? Id { get; set; } = KeyGenerator.GetUniqueKey(6);
+    public string? Texto { get; set; }
+    public BotaoStyle Estilo { get; set; } = new();
+    public string? Url { get; set; }
+
+    public BotaoComponent () { }
+
+    public BotaoComponent (string? id, string texto, BotaoStyle estilo, string? url)
+    {
+        Id = id;
+        Texto = texto;
+        Estilo = estilo;
+        Url = url;
+    }
+
+    public BotaoComponent (string texto, BotaoStyle estilo, string? url)
+    {
+        Texto = texto;
+        Estilo = estilo;
+        Url = url;
+    }
+}
+
+public class BotaoStyle
+{
+    public string? Classe { get; set; } = "btn btn-primary btn-sm";
+    public bool Submit { get; set; }
+    
+    public BotaoStyle () { }
+
+    public BotaoStyle (string? classe, bool submit = false)
+    {
+        Classe = classe;
+        Submit = submit;
+    }
+}
---
+++ EasyERP/Web/Models/Components/DropAnos.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Models.Components;
+
+public class DropAnos
+{
+    public int AnoInicio { get; set; } = 2025;
+    public int? AnoSelecionado { get; set; }
+}
