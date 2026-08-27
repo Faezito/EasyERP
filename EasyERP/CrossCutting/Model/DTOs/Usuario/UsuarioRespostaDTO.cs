e3e38a9eb4054a2ab8f013e225f0cd1eb3b7a933
Matheus Faé <matheusfae@gmail.com>
2026-08-27 17:38:20 -0300

    Adicionados módulos

--- EasyERP/CrossCutting/Model/DTOs/Usuario/UsuarioRespostaDTO.cs
+++ EasyERP/CrossCutting/Model/DTOs/Usuario/UsuarioRespostaDTO.cs
@@ -3,15 +3,14 @@ using CrossCutting.Model.Enums;
 using Model.DTOs.Endereco;
 using Model.DTOs.PessoaFisica;
 
-namespace Model.DTOs.Usuario
+namespace Model.DTOs.Usuario;
+
+public class UsuarioRespostaDTO : EntidadeAuditavel
 {
-    public class UsuarioRespostaDTO : EntidadeAuditavel
-    {
-        public Guid PublicId { get; set; }
-        public string NomeUsuario { get; set; } = string.Empty;
-        public Perfil Perfil { get; set; }
-        public EnderecoRespostaDTO? Endereco { get; set; }
-        public PessoaFisicaRespostaDTO? Pessoa { get; set; }
-        public List<ModuloDTO> Acessos { get; set; } = new();
-    }
+    public Guid PublicId { get; set; }
+    public string NomeUsuario { get; set; } = string.Empty;
+    public Perfil Perfil { get; set; }
+    public EnderecoRespostaDTO? Endereco { get; set; }
+    public PessoaFisicaRespostaDTO? Pessoa { get; set; }
+    public List<UsuarioModuloDTO> Modulos { get; set; } = [];
 }
