1ce45562c18e75e23f33ab152398891fccc84af3
Matheus Faé <matheusfae@gmail.com>
2026-08-27 11:46:59 -0300

    Criada classe genérica para estilos de componentes comuns

---
+++ EasyERP/Web/Models/Components/Estilo.cs
@@ -0,0 +1,9 @@
+﻿using Biblioteca;
+
+namespace Web.Models.Components;
+
+public class Estilo
+{
+    public string? Id { get; set; } = KeyGenerator.GetUniqueKey(4);
+    public string? Classe { get; set; }
+}
