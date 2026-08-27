754c94a1b82006b49e79d380cd60d4713e9aff94
Matheus Faé <matheusfae@gmail.com>
2026-08-27 11:45:49 -0300

    Renomeação da classe e arquivo do Componente de Navbar, e conclusão das propriedades e view dinâmica

--- EasyERP/Web/Models/Components/Navbar.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Web.Models.Components;
-
-public class Navbar
-{
-    public int ContagemAtualizacoes { get; set; }
-    public List<string>? ListaAtualizacoes { get; set; }
-}
---
+++ EasyERP/Web/Views/Shared/Components/NavbarComponent/NavbarComponent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models.Components;
+
+namespace Web.Views.Shared.Components.NavbarComponent
+{
+    public class NavbarComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Navbar navbar)
+        {
+            return View("Default", navbar);
+        }
+
+        /*
+            @await Component.InvokeAsync("NavbarComponent", new Navbar())
+        */
+    }
+}
+
+public class Navbar
+{
+    public string? Titulo { get; set; }
+    public string? Logo { get; set; }
+    public string? Background { get; set; }
+    public string? Cor { get; set; }
+    public List<NavbarLink> Links { get; set; } = [];
+    public int ContagemAtualizacoes { get; set; }
+    public List<string>? ListaAtualizacoes { get; set; }
+}
+
+public class NavbarLink
+{
+    public NavbarLink()
+    {
+        Estilo = new Estilo { Classe = "nav-link text-light" };
+    }
+
+    public required string Texto { get; set; }
+    public required string Url { get; set; }
+    public Estilo Estilo { get; set; } = new();
+}
+
+
+//<a class="nav-link text-light" asp-controller="Menu" asp-action="Index">MENU</a>
