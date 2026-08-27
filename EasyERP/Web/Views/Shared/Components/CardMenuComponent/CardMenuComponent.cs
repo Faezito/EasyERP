d0d952c529df189afe0bfe0634480b5f107d2233
Matheus Faé <matheusfae@gmail.com>
2026-08-27 17:37:23 -0300

    Criar componente dinâmico de CardMenu

---
+++ EasyERP/Web/Views/Shared/Components/CardMenuComponent/CardMenuComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models.Components;
+
+namespace Web.Views.Shared.Components.CardMenuComponent;
+
+public class CardMenuComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(CardMenu component)
+    {
+        return View("Default", component);
+    }
+}
+
+public class CardMenu
+{
+    public string? Texto { get; set; }
+    public string? Imagem { get; set; }
+    public Estilo Estilo { get; set; } = new Estilo { Classe = "" };
+    public string? Url { get; set; }
+}
