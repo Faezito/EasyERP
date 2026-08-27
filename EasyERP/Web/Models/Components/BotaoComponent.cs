79024ba1e66a4c5f3e785fe88492ed00ea2de096
Matheus Faé <matheusfae@gmail.com>
2026-08-27 11:46:44 -0300

    Adicionadas propriedades e construtores

--- EasyERP/Web/Models/Components/BotaoComponent.cs
+++ EasyERP/Web/Models/Components/BotaoComponent.cs
@@ -8,10 +8,11 @@ public class BotaoComponent
     public string? Texto { get; set; }
     public BotaoStyle Estilo { get; set; } = new();
     public string? Url { get; set; }
+    public string? OnClick { get; set; }
 
-    public BotaoComponent () { }
+    public BotaoComponent() { }
 
-    public BotaoComponent (string? id, string texto, BotaoStyle estilo, string? url)
+    public BotaoComponent(string? id, string texto, BotaoStyle estilo, string? url)
     {
         Id = id;
         Texto = texto;
@@ -19,22 +20,32 @@ public class BotaoComponent
         Url = url;
     }
 
-    public BotaoComponent (string texto, BotaoStyle estilo, string? url)
+    public BotaoComponent(string texto, BotaoStyle estilo, string? url)
     {
         Texto = texto;
         Estilo = estilo;
         Url = url;
     }
+
+    public BotaoComponent(string? url, string? onclick)
+    {
+        Texto = "<i class=\"fa-solid fa-reply\"></i>&nbsp; Voltar";
+        Estilo = new BotaoStyle("btn btn-secondary btn-sm col-auto");
+        Url = url;
+        OnClick = onclick;
+    }
 }
 
-public class BotaoStyle
+public class BotaoStyle : Estilo
 {
-    public string? Classe { get; set; } = "btn btn-primary btn-sm";
     public bool Submit { get; set; }
-    
-    public BotaoStyle () { }
 
-    public BotaoStyle (string? classe, bool submit = false)
+    public BotaoStyle()
+    {
+        Classe = "btn btn-primary btn-sm";
+    }
+
+    public BotaoStyle(string? classe, bool submit = false)
     {
         Classe = classe;
         Submit = submit;
