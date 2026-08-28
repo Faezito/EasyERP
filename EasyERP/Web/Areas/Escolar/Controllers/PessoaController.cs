8a83f5e49192e342f47e1f44b1266e3ea6085b16
Matheus Faé <matheusfae@gmail.com>
2026-08-28 19:26:52 -0300

    Refactor: removido parametro token que é obtido nas requisições automaticamente

--- EasyERP/Web/Areas/Escolar/Controllers/PessoaController.cs
+++ EasyERP/Web/Areas/Escolar/Controllers/PessoaController.cs
@@ -1,11 +1,12 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Model.DTOs.Escolar.Pessoa;
 using Web.Areas.Escolar.Services;
 using Web.Libraries.Filtros;
+using Web.Libraries.Http;
+using Web.Libraries.Validacoes;
 using Web.Models.TabelaDinamica;
 using Web.Views.Shared.Components.TabelaDinamica;
 
@@ -28,10 +29,7 @@ public class PessoaController(IPessoaServices pessoaServices, IMapper mapper) : 
     [HttpPost]
     public async Task<IActionResult> Listar()
     {
-        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        var token = result?.Properties?.GetTokenValue("access_token");
-        
-        var pessoas = await _pessoaServices.Listar(token);
+        var pessoas = await _pessoaServices.Listar();
         return ViewComponent(typeof(TabelaDinamica), new TabelaDinamicaModel(pessoas, Url.Action(nameof(Edicao)), Url.Action(nameof(Deletar)), true));
     }
 
@@ -44,31 +42,32 @@ public class PessoaController(IPessoaServices pessoaServices, IMapper mapper) : 
     [HttpPost]
     public async Task<IActionResult> Cadastrar(PessoaCadastroDTO dto)
     {
-        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        var token = result?.Properties?.GetTokenValue("access_token");
+        ModelStateHelper.ValidarModelState(ModelState);
 
-        await _pessoaServices.Cadastrar(dto, token);
+        await _pessoaServices.Cadastrar(dto);
         return Json(new { success = true });
     }
 
     [HttpGet]
     public async Task<IActionResult> Edicao(Guid id)
     {
-        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        var token = result?.Properties?.GetTokenValue("access_token");
-
-        var pessoa = await _pessoaServices.Obter(id, token);
+        var pessoa = await _pessoaServices.Obter(id);
         var pessoaEdicao = _mapper.Map<PessoaAtualizacaoDTO>(pessoa);
         return View(pessoaEdicao);
     }
 
     [HttpPost]
+    public async Task<IActionResult> Edicao(PessoaAtualizacaoDTO dto)
+    {
+        ModelStateHelper.ValidarModelState(ModelState);
+        await _pessoaServices.Atualizar(dto);
+        return View();
+    }
+
+    [HttpPost]
     public async Task<IActionResult> Deletar(Guid id)
     {
-        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        var token = result?.Properties?.GetTokenValue("access_token");
-
-        await _pessoaServices.Deletar(id, token);
+        await _pessoaServices.Deletar(id);
         return Json(new { success = true, reloadPage = false });
     }
 }
--- EasyERP/Web/Areas/Escolar/Services/PessoaServices.cs
+++ EasyERP/Web/Areas/Escolar/Services/PessoaServices.cs
@@ -6,41 +6,41 @@ namespace Web.Areas.Escolar.Services;
 
 public interface IPessoaServices
 {
-    Task Cadastrar(PessoaCadastroDTO pessoaCadastro, string? token);
-    Task Atualizar(PessoaAtualizacaoDTO pessoa, string? token);
-    Task<List<PessoaTabela>> Listar(string? token);
-    Task<PessoaRespostaDTO> Obter(Guid id, string? token);
-    Task Deletar(Guid id, string? token);
+    Task Cadastrar(PessoaCadastroDTO pessoaCadastro);
+    Task Atualizar(PessoaAtualizacaoDTO pessoa);
+    Task<List<PessoaTabela>> Listar();
+    Task<PessoaRespostaDTO> Obter(Guid id);
+    Task Deletar(Guid id);
 }
 
 public class PessoaServices(IClientFactory http) : IPessoaServices
 {
     private readonly IClientFactory _http = http;
 
-    public async Task Cadastrar(PessoaCadastroDTO pessoaCadastro, string? token)
+    public async Task Cadastrar(PessoaCadastroDTO pessoaCadastro)
     {
-        await _http.Post("api/escolar/Pessoa/cadastro", pessoaCadastro, new Api { Token = token, Url = "https://localhost:44380/" });
+        await _http.Post("api/escolar/Pessoa/cadastro", pessoaCadastro, new Api { Url = "https://localhost:44380/" });
     }
 
-    public async Task Atualizar(PessoaAtualizacaoDTO pessoa, string? token)
+    public async Task Atualizar(PessoaAtualizacaoDTO pessoa)
     {
-        await _http.Put("api/escolar/Pessoa/atualizacao", pessoa, new Api { Token = token, Url = "https://localhost:44380/" });
+        await _http.Put("api/escolar/Pessoa/atualizacao", pessoa, new Api { Url = "https://localhost:44380/" });
     }
 
-    public async Task<List<PessoaTabela>> Listar(string? token)
+    public async Task<List<PessoaTabela>> Listar()
     {
-        var ret = await _http.Get<List<PessoaRespostaDTO>>("api/escolar/Pessoa/listar", new Api { Token = token, Url = "https://localhost:44380/" });
+        var ret = await _http.Get<List<PessoaRespostaDTO>>("api/escolar/Pessoa/listar", new Api { Url = "https://localhost:44380/" });
 
         var pessoas = PessoaTabela.MapearParaTabela(ret);
         return pessoas;
     }
-    public async Task<PessoaRespostaDTO> Obter(Guid id, string? token)
+    public async Task<PessoaRespostaDTO> Obter(Guid id)
     {
-        return await _http.Get<PessoaRespostaDTO>($"api/escolar/Pessoa/{id}", new Api { Token = token, Url = "https://localhost:44380/" });
+        return await _http.Get<PessoaRespostaDTO>($"api/escolar/Pessoa/{id}", new Api { Url = "https://localhost:44380/" });
     }
 
-    public async Task Deletar(Guid id, string? token)
+    public async Task Deletar(Guid id)
     {
-        var ret = await _http.Delete<HttpResponse>($"api/escolar/Pessoa/deletar?publicId={id}", new Api { Token = token, Url = "https://localhost:44380/" });
+        var ret = await _http.Delete<HttpResponse>($"api/escolar/Pessoa/deletar?publicId={id}", new Api { Url = "https://localhost:44380/" });
     }
 }
