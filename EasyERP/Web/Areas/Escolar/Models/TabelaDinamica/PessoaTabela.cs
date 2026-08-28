7ce773c888fa2eb2c73e077c22138267806ff9ec
Matheus Faé <matheusfae@gmail.com>
2026-08-28 14:16:57 -0300

    Criada entidade de tabela dinamica para pessoas

---
+++ EasyERP/Web/Areas/Escolar/Models/TabelaDinamica/PessoaTabela.cs
@@ -0,0 +1,43 @@
+﻿using Model.DTOs.Escolar.Pessoa;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Areas.Escolar.Models.TabelaDinamica;
+
+public class PessoaTabela
+{
+    [Display(Name = "Id")]
+    public Guid Id { get; set; }
+
+    [Display(Name = "Nome")]
+    public string Name { get; set; }
+
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Display(Name = "Telefone")]
+    public string Telefone { get; set; }
+
+    [Display(Name = "Endereço")]
+    public string Endereco { get; set; }
+
+
+    public static List<PessoaTabela> MapearParaTabela(List<PessoaRespostaDTO> pessoasRespostaDTO)
+    {
+        var pessoasTabela = new List<PessoaTabela>();
+        foreach (var pessoaDTO in pessoasRespostaDTO)
+        {
+            var pessoa = new PessoaTabela
+            {
+                Id = pessoaDTO.PublicId,
+                Name = pessoaDTO.NomeCompleto,
+                Email = pessoaDTO.Email,
+                Telefone = pessoaDTO.Telefone,
+                Endereco = $"{pessoaDTO.Endereco.Logradouro} {pessoaDTO.Endereco.Numero}, {pessoaDTO.Endereco.Bairro} - {pessoaDTO.Endereco.Cidade}, {pessoaDTO.Endereco.Estado}"
+            };
+
+            pessoasTabela.Add(pessoa);
+        }
+
+        return pessoasTabela ?? new();
+    }
+}
