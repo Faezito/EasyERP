109b5cdd82725e529ed47d9dc537b6ee629a6fbc
Matheus Faé <matheusfae@gmail.com>
2026-08-28 14:19:31 -0300

    Adicionado atributo de validação de acesso

--- EasyERP/Web/Areas/Escolar/Controllers/HomeController.cs
+++ EasyERP/Web/Areas/Escolar/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Libraries.Filtros;
 
 namespace Web.Areas.Escolar.Controllers;
 
 [Area("Escolar")]
+[ValidateHttpRefererAttributes]
 public class HomeController : Controller
 {
     [HttpGet]
