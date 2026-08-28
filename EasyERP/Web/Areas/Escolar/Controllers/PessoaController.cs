67cabb02ca641e52730b10b4c8ba07b2e67bc851
Matheus Faé <matheusfae@gmail.com>
2026-08-28 14:18:20 -0300

    Adicionado CRUD de pessoas base ao front-end

---
+++ EasyERP/Web/Areas/Escolar/Controllers/PessoaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Model.DTOs.Escolar.Pessoa;
+using Web.Areas.Escolar.Services;
+using Web.Libraries.Filtros;
+using Web.Models.TabelaDinamica;
+using Web.Views.Shared.Components.TabelaDinamica;
+
+namespace Web.Areas.Escolar.Controllers;
+
+[Area("Escolar")]
+[Authorize]
+[ValidateHttpRefererAttributes]
+public class PessoaController(IPessoaServices pessoaServices) : Controller
+{
+    private readonly IPessoaServices _pessoaServices = pessoaServices;
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Listar()
+    {
+        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        var token = result?.Properties?.GetTokenValue("access_token");
+        
+        var pessoas = await _pessoaServices.Listar(token);
+        return ViewComponent(typeof(TabelaDinamica), new TabelaDinamicaModel(pessoas, "", Url.Action(nameof(Deletar)), true));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Cadastrar(PessoaCadastroDTO dto)
+    {
+        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        var token = result?.Properties?.GetTokenValue("access_token");
+
+        await _pessoaServices.Cadastrar(dto, token);
+        return Json(new { success = true });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Deletar(Guid id)
+    {
+        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        var token = result?.Properties?.GetTokenValue("access_token");
+
+        await _pessoaServices.Deletar(id, token);
+        return Json(new { success = true, reloadPage = false });
+    }
+}
---
+++ EasyERP/Web/Areas/Escolar/Services/PessoaServices.cs
@@ -0,0 +1,37 @@
+﻿using Bibliotecas.Http;
+using Model.DTOs.Escolar.Pessoa;
+using Web.Areas.Escolar.Models.TabelaDinamica;
+
+namespace Web.Areas.Escolar.Services;
+
+public interface IPessoaServices
+{
+    Task Cadastrar(PessoaCadastroDTO pessoaCadastro, string? token);
+    Task<List<PessoaTabela>> Listar(string? token);
+    Task Deletar(Guid id, string? token);
+}
+
+public class PessoaServices(IClientFactoryPost post, IClientFactoryGet get, IClientFactoryDelete delete) : IPessoaServices
+{
+    private readonly IClientFactoryPost _post = post;
+    private readonly IClientFactoryGet _get = get;
+    private readonly IClientFactoryDelete _delete = delete;
+
+    public async Task Cadastrar(PessoaCadastroDTO pessoaCadastro, string? token)
+    {
+        await _post.Post("api/escolar/Pessoa/cadastro", pessoaCadastro, new Api { Token = token, Url = "https://localhost:44380/" });
+    }
+
+    public async Task<List<PessoaTabela>> Listar(string? token)
+    {
+        var ret = await _get.Get<List<PessoaRespostaDTO>>("api/escolar/Pessoa/listar", new Api { Token = token, Url = "https://localhost:44380/" });
+
+        var pessoas = PessoaTabela.MapearParaTabela(ret);
+        return pessoas;
+    }
+
+    public async Task Deletar(Guid id, string? token)
+    {
+        var ret = await _delete.Delete<HttpResponse>($"api/escolar/Pessoa/deletar?publicId={id}", new Api { Token = token, Url = "https://localhost:44380/" });
+    }
+}
