ef7927c38e62c6e657629dac6398c860ad61f8a1
Matheus Faé <matheusfae@gmail.com>
2026-09-01 17:34:55 -0300

    Adicionado método de obter e alterados alguns parâmetros

--- EasyERP/Auth.Servicos/ModuloServicos.cs
+++ EasyERP/Auth.Servicos/ModuloServicos.cs
@@ -8,9 +8,10 @@ namespace Auth.Servicos;
 
 public interface IModuloServicos : ICRUDGenerico<Modulo>
 {
+    Task<ModuloDTO> ObterPorId(int id);
     Task<List<ModuloDTO>> ListarTodos();
-    Task Cadastrar(Modulo modulo);
-    new Task Atualizar(Modulo modulo);
+    Task Cadastrar(ModuloCadastroDTO modulo);
+    Task Atualizar(ModuloDTO modulo);
     Task Deletar(int id);
 }
 
@@ -18,23 +19,25 @@ public class ModuloServicos(AppDbContext db, IMapper mapper) : CRUDGenerico<Modu
 {
     private readonly IMapper _mapper = mapper;
 
-    public async Task Cadastrar(Modulo modulo)
+    public async Task Cadastrar(ModuloCadastroDTO modulo)
     {
-        Adicionar(modulo);
+        var novoModulo = _mapper.Map<Modulo>(modulo);
+        Adicionar(novoModulo);
         await SalvarAsync();
     }
 
-    public new async Task Atualizar(Modulo modulo)
+    public async Task Atualizar(ModuloDTO modulo)
     {
         var existente = await _dbSet.FirstOrDefaultAsync(x => x.Id == modulo.Id);
         if (existente == null)
             throw new Exception("Módulo não encontrado");
 
-        existente.Nome = modulo.Nome;
-        existente.Descricao = modulo.Descricao;
+        existente.Nome = modulo.Nome ?? existente.Nome;
+        existente.Descricao = modulo.Descricao ?? existente.Descricao;
         existente.Ativo = modulo.Ativo;
-        existente.BaseUrl = modulo.BaseUrl;
-        existente.ModuloPaiId = modulo.ModuloPaiId;
+        existente.BaseUrl = modulo.BaseUrl ?? existente.BaseUrl;
+        existente.Imagem = modulo.Imagem ?? existente.Imagem;
+        existente.ModuloPaiId = modulo.ModuloPaiId ?? existente.ModuloPaiId;
 
         await SalvarAsync();
     }
@@ -53,5 +56,11 @@ public class ModuloServicos(AppDbContext db, IMapper mapper) : CRUDGenerico<Modu
     {
         var modulos = await _dbSet.ToListAsync();
         return _mapper.Map<List<ModuloDTO>>(modulos);
+    }
+
+    public async Task<ModuloDTO> ObterPorId(int id)
+    {
+        var modulo = await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
+        return _mapper.Map<ModuloDTO>(modulo);
     }
 }
