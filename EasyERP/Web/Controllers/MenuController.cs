58dc452906a89d1d93cf3363a71a5329d3930a7b
Matheus Faé <matheusfae@gmail.com>
2026-09-01 17:35:15 -0300

    Criado Menu de admin

---
+++ EasyERP/Web/Controllers/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[Authorize]
+public class MenuController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Admin()
+    {
+        return View();
+    }
+}
