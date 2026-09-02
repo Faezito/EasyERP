265bdf86feee9ab6c9b1e2dac242c6eefd4adff5
Matheus Faé <matheusfae@gmail.com>
2026-09-02 16:13:25 -0300

    Criado componente genérico para dropdown

---
+++ EasyERP/Web/Views/Shared/Components/DropdownDinamicoComponent/DropdownDinamicoComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.Views.Shared.Components.DropdownDinamicoComponent;
+
+public class DropdownDinamicoComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(DropDown model)
+    {
+        return View(model);
+    }
+}
+
+public class DropDown
+{
+    public List<SelectListItem> Items { get; set; } = new List<SelectListItem>();
+    public int? Selecionado { get; set; }
+}
+
+/*
+    @await Component.InvokeAsync("DropdownDinamicoComponent", new { items = lst })
+*/
